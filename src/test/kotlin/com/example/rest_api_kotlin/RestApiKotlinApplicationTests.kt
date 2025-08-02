751d02ee1c9ad71cd58971455f2fe29d8b4d75e1
Yong Jie <yongjie.wong@hiseven.com>
2025-08-03 01:37:26 +0800

    basic api setup

---
+++ src/test/kotlin/com/example/rest_api_kotlin/RestApiKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.rest_api_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
